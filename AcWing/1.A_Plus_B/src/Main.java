c73d6a0a35b9590c6e3ec2f3c87d47ca42367a4f
KAR1SEVEN <kariseven323@gmail.com>
2024-07-05 00:21:33 +0800

    A+B

---
+++ AcWing/1.A_Plus_B/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * @author KAR1SEVEN
+ */
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a,b;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println(a+b);
+    }
+}
